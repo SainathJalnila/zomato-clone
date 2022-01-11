c863b6466755811d11955091a13eb516f772e774
Sainath7440 <Saijalanila7070@gmail.com>
2022-01-12 03:18:29 +0530

    done

---
+++ app/index.js
@@ -0,0 +1,25 @@
+let express  =  require('express');
+let routes =  require('./route/route.js');
+let mongo = require('mongoose');
+let cors = require('cors');
+
+let app = express();
+const port = process.env.PORT || 7845; 
+
+const atlasDbUrl = 'mongodb+srv://SainathJalnila:Honda7440@cluster0.bjk7t.mongodb.net/ZomatoClone?retryWrites=true&w=majority';
+
+
+app.use(express.json());
+app.use(cors());
+app.use('/api',  routes);
+
+mongo.connect( atlasDbUrl,{
+    
+})
+ .then(res =>
+    {
+        app.listen(port,()=>
+{
+    console.log(`Server hased started ${port} `);
+})
+    }).catch(err => console.log(err));
